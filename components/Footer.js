42f077429a8c5072ae655ab81a71b0e281136c92
Angel Lopez <a.o.lopez.cruz@gmail.com>
2021-08-09 15:00:56 -0500

    Navbar y Footer creados

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+
+  let dateFull = new Date()
+  let onlyYear = dateFull.getFullYear()
+
+  return (
+    <footer className="w-full py-6 bg-green-200">
+      <p className="text-center font-medium text-lg text-gray-700">©Doctora​ Natividad Gutiérrez Chong. Todos los derechos reservados. {onlyYear}</p>
+    </footer>
+  )
+}
