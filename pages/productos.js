9ba6186811dc4a0ecd0a68ba7e681356cbce2c6f
Angel Lopez <a.o.lopez.cruz@gmail.com>
2021-08-12 20:28:19 -0500

    paginas productos digitales terminada

--- pages/productos.js
+++ pages/productos.js
@@ -6,7 +6,7 @@ export default function productos() {
       </div>
       <section>
         <div>
-          <h2>Juventidades</h2>
+          <h2 className="text-center">Juventidades</h2>
           <p className="text-justify"><span className="font-bold">Jóvenes de identidades diversas en dinamicas metropolitanas Juventidades</span>, es una plataforma que presenta un esfuerzo creativo, disciplinario y metodológico,
             usando métodos cualitativos y cuantitativos para mostrar evidencias de cómo vive la juventud en la zona metropolitana. Por supuesto que la información y análisis que aquí presentamos no es exhaustiva, pues ningún esfuerzo sería suficiente para mostrar
             una vasta diversidad que aqui buscamos analizar a través de un enfoque interseccional. Hemos recogido y sistematizado información de acuerdo con tres grupos de edad, sexo, género, clase social, color de piel y etnicidad. Un hallazgo de este estudio ha sido
@@ -34,7 +34,7 @@ export default function productos() {
       </section>
       <section>
         <div>
-          <h2>Pluriverso Digital</h2>
+          <h2 className="text-center">Pluriverso Digital</h2>
           <p className="text-justify">La <span className="font-bold">Plataforma Digital sobre Estudios de la Diversidad Cultural en México Pluriverso Digital</span> es una herramienta digital que ofrece un panorama de la complejidad de la cultura y sus interconexiones de esos seis conceptos o ejes relacionados con el estudio de las culturas, la era del reconocimiento a minorías, y las políticas públicas. Estos ejes son: <span className="font-bold">identidad, interseccionalidad, interculturalidad, interculturalismo, multiculturalidad y multiculturalismo.</span> Cada uno de ellos incluye un mapa mental, bibliografía, imágenes y vídeos.</p>
           <h3>Objetivos</h3>
           <p className="text-justify">En la actualidad, los términos de interculturalidad, interculturalismo, multiculturalidad, multiculturalismo, interseccionalidad e identidad se usan ampliamente en el discurso cotidiano, político, académico, educativo, mediático, diplomático y cultural. Sin embargo, como ocurre frecuentemente, a mayor utilización de estos conceptos, menor claridad sobre sus significados, semejanzas, diferencias o interconexiones. En <span className="font-bold">Pluriverso Digital</span> buscamos intervenir de manera innovadora e interactiva en los conceptos aludidos, recurriendo a las actuales redes socio-digitales de información, de comunicación, y del lenguaje audiovisual.</p>
@@ -47,17 +47,25 @@ export default function productos() {
           </ul>
         </div>
         <div className="flex w-full justify-center">
-        <picture>
+          <picture>
             <img className="rounded-md" src="/PluriversoDigital.png" alt="imagen portada sitio web Pluriverso Digital" />
           </picture>
         </div>
       </section>
       <section>
         <div>
-          <h2>SICETNO</h2>
+          <h2 className="text-center">SICETNO</h2>
           <p className="text-justify"><span className="font-bold">SICETNO Sistema de consulta de organizaciones indígenas y conflictos étnicos en las Americas</span>, es una plataforma digital desarrollada con herramientas innovadoras para vincular la investigación en ciencias sociales con las tecnologías de información sobre temas de democracia y derechos humanos en el continente.</p>
           <p className="text-justify">SICETNO incluye dos bases de datos: <span className="font-bold">Organizaciones indígenas de América Latina (ORGINDAL*)</span> y <span className="font-bold">Conflictos étnicos y nacionalismos en las Américas (CETNA*)</span></p>
           <h3>Objetivos</h3>
+          <p className="text-justify">Crear y desarrollar un sistema de consulta digital con información actualizada de las organizaciones de los pueblos indígenas y de los conflictos étnicos que ocurren en las Américas.</p>
+          <p className="text-justify">Ofrecer una plataforma digital en la que se incluyen las propuestas y modelos de autonomía y libre determinación que han diseñado y ejecutan algunos pueblos indios de México, Ecuador y Bolivia.</p>
+          <p className="text-justify">Construir y desarrollar una plataforma digital que pueda ser accesible a las organizaciones indígenas cuando la técnica de negociación política, tal como el cabildeo o el consenso, sean requeridos en asuntos de interés jurídicos indígenas.</p>
+          <p className="text-justify">Crear una plataforma que posibilite la actualización y retroalimentación de la información sobre acción colectiva y conflictividad indígenas, y que sirva para ampliar su difusión dentro y fuera del país, aprovechando las posibilidades que ofrece la globalización digital de la Internet.</p>
+          <p className="text-justify">Ampliar y fortalecer la capacidad política de los pueblos indígenas. La plataforma digital permitirá el acceso de las minorías étnicas a la representación política, al contar con medios de intercomunicación y actualización de información. La representación política de minorías se ha convertido en un indicador para evaluar la calidad de la democracia en los sistemas políticos contemporáneos.</p>
+          <p className="text-justify">Facilitar el acceso a la información y su socialización entre los diferentes actores sociales que involucren intereses étnicos, y de esta manera contribuir a que el trabajo desarrollado en las organizaciones logre ser visible en cuanto a sus planteamientos y propuestas con respecto a la autonomía, a la libre determinación, entre otros. La plataforma también muestra los tipos de conflictividad étnica en las Américas e identifica los daños y amenazas más frecuentes que ponen en riesgo a las poblaciones indígenas y a los territorios que habitan.</p>
+          <p className="text-justify">Contribuir al desarrollado de la cultura política de los pueblos indígenas por medio de la tecnología que hará posible un acercamiento entre las organizaciones indígenas y el gobierno, con el fin de conocer el diseño y la ejecución del marco jurídico mexicano y su vinculación con las demandas de los pueblos indígenas. El funcionamiento de la plataforma digital permitirá fortalecer las iniciativas de los pueblos originarios en materia de consultas públicas.</p>
+          <p className="text-justify"></p>
           <ul>
             <li>Año de inicio: 2006</li>
             <li>Última actualización: 2021</li>
@@ -66,13 +74,21 @@ export default function productos() {
             <li>Soporte técnico: Oracle APEX</li>
           </ul>
         </div>
-        <div></div>
+        <div className="flex w-full justify-center">
+          <picture>
+            <img className="rounded-md" src="/SICETNO.png" alt="imagen portada sitio web SICETNO" />
+          </picture>
+        </div>
       </section>
       <section>
         <div>
-          <h2>Humanindex</h2>
+          <h2 className="text-center">Humanindex</h2>
         </div>
-        <div></div>
+        <div className="flex w-full justify-center">
+          <picture>
+            <img className="rounded-md" src="/Humanindex.png" alt="imagen portada sitio web Humanindex" />
+          </picture>
+        </div>
       </section>
     </main>
   )
